1c6b14dc90cdc50b6dbac461c346ff73a11ddc3f
Ahmed Elattar <ahmedelattar73@gmail.com>
2022-08-14 04:23:33 +0200

    Filter historical data by date.

--- app/Bussiness/HistoricalData/HistoricalData.php
+++ app/Bussiness/HistoricalData/HistoricalData.php
@@ -6,6 +6,7 @@ use App\DTO\HistoricalDataRequestTransfer;
 use App\Mail\HistoricalDataReport;
 use App\Repositories\CompanyRepository;
 use App\Services\YhFinanceService;
+use Carbon\Carbon;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Mail;
 
@@ -27,6 +28,8 @@ class HistoricalData
             $historicalDataRequestTransfer->getSymbol()
         );
 
+        $historicalDataCollection = $this->filterDataByDate($historicalDataCollection, $historicalDataRequestTransfer);
+
         $companyTransfer = app(CompanyRepository::class)
             ->findBySymbol($historicalDataRequestTransfer->getSymbol());
 
@@ -37,5 +40,40 @@ class HistoricalData
             ));
 
         return $historicalDataCollection;
+    }
+
+    /**
+     * @param Collection $historicalDataCollection
+     * @param HistoricalDataRequestTransfer $historicalDataRequestTransfer
+     *
+     * @return Collection
+     */
+    protected function filterDataByDate(Collection $historicalDataCollection, HistoricalDataRequestTransfer $historicalDataRequestTransfer): Collection
+    {
+        $startDate = Carbon::create($historicalDataRequestTransfer->getStartDate());
+        $endDate = Carbon::create($historicalDataRequestTransfer->getEndDate());
+
+        foreach ($historicalDataCollection as $elementKey => $element) {
+            if (!empty($element->getDate())) {
+                $originalSate = Carbon::createFromTimestamp($element->getDate());
+                if ($this->isOutOfDateRange($originalSate, $startDate, $endDate)) {
+                    unset($historicalDataCollection[$elementKey]);
+                }
+            }
+        }
+
+        return $historicalDataCollection;
+    }
+
+    /**
+     * @param Carbon $originalSate
+     * @param Carbon $startDate
+     * @param Carbon $endDate
+     *
+     * @return bool
+     */
+    protected function isOutOfDateRange(Carbon $originalSate, Carbon $startDate, Carbon $endDate): bool
+    {
+        return $originalSate->gte($endDate) || $originalSate->lte($startDate);
     }
 }
